eecf39f473306e27025780ed0aa942aafc451524
Fabian Kabus <f.kabus@tu-bs.de>
2018-10-31 13:26:45 +0100

    render roi traces to mp4

---
+++ zebrafishframework/rendering.py
@@ -0,0 +1,26 @@
+import numpy as np
+from pyprind import prog_percent
+
+import moviepy.editor as mpy
+
+FFMPEG_BIN = 'ffmpeg'
+
+def orthogonal(rois, traces, color_func, ts, shape):
+    activity = np.zeros((np.alen(ts),) + tuple(shape) + (3,), dtype=np.uint8)
+
+    for i, t in prog_percent(list(enumerate((ts)))):
+        for roi_id, (roi, trace) in enumerate(zip(rois, traces)):
+            x, y = roi['x'], roi['y']
+            activity[i, x, y] = color_func(trace[t])
+
+    return activity
+
+
+def to_file(filename, video, fps=30):
+
+    def make_frame(t):
+        frame = video[int(t*fps)]
+        return frame
+
+    clip = mpy.VideoClip(make_frame, duration=np.alen(video)/fps)
+    clip.write_videofile(filename, fps=fps)
