e16b378ab5f5e1823a9f2d6da283475ad5dc4347
Fabian Kabus <f.kabus@tu-bs.de>
2018-11-19 15:58:50 +0100

    segmentation stuff

--- zebrafishframework/segmentation.py
+++ zebrafishframework/segmentation.py
@@ -1,14 +1,27 @@
 import numpy as np
 from skimage.draw import circle
-from skimage.feature import match_template, peak_local_max
+from skimage.feature import match_template, peak_local_max, blob_log
+from sklearn.preprocessing import normalize
 
 
-def std(trace, anatomy, shifts=None):
+def valid_frames(invalid_frames, length):
+    frames = list(range(length))
+    if invalid_frames:
+        for f in invalid_frames:
+            frames.remove(f)
+
+    return frames
+
+
+def std(stack, valid_frames=None):
     anatomy_std = []
 
-    for plane in range(anatomy.shape[0]):
+    if not valid_frames:
+        valid_frames = np.arange(np.alen(stack))
+
+    for plane in range(stack.shape[1]):
         #    anatomy_std.append(np.std(trace[displacement[plane]<30, plane], axis=0))
-        anatomy_std.append(np.std(trace[:, plane, ...], axis=0))
+        anatomy_std.append(np.std(stack[valid_frames, plane, ...], axis=0))
 
     anatomy_std = np.array(anatomy_std, dtype=np.float32)
     return anatomy_std
@@ -38,6 +51,32 @@ def find_rois_andreas(anatomy_std, template, trace, mask=None):
     return rois
 
 
+def get_traces(stack, rois):
+    traces = []
+    for roi in rois:
+        x, y, z, r = list(map(int, roi))
+        stencil = circle(x, y, r, stack[0][z].shape)
+        trace = np.array(stack[:, z, stencil[0], stencil[1]].mean(1))
+        traces.append(trace)
+    return np.asarray(traces)
+
+
+def find_rois_template(img, template, peakMinDistance=2, peakRelThreshold=.2, minIntensity=20):
+    m = match_template(img, template, pad_input=True)
+    plm = peak_local_max(m, min_distance=peakMinDistance, threshold_rel=peakRelThreshold)
+    return np.asarray([(y, x) for x, y in plm if (img[x, y] > minIntensity)])
+
+
+def find_rois_blob(nda, maxRadius=3, sizeIters=30, threshold=1.5, overlap=0, minIntensity=20):
+    rois = []
+    for z, img in enumerate(nda):
+        blobs = blob_log(normalize(img), max_sigma=maxRadius*0.70710678118, # sigma = radius/sqrt(2)
+                         num_sigma=sizeIters, threshold=threshold/np.max(img), overlap=overlap)
+        blobs = np.asarray([(y, x, z, r) for x, y, r in blobs if (img[int(x), int(y)] > minIntensity)])
+        rois.extend(blobs)
+    return rois
+
+
 def draw_rois(rois, anatomy_std, color_func=None):
     roi_map = np.zeros(anatomy_std.shape + (3,), dtype=np.uint8)
 
@@ -45,9 +84,7 @@ def draw_rois(rois, anatomy_std, color_func=None):
         roi_map[plane, :, :] = (anatomy_std[plane][..., None] / np.max(anatomy_std[plane]) * 255).astype(np.uint8)
 
     for roi_id, roi in enumerate(rois):
-        x = roi['x']
-        y = roi['y']
-        z = roi['z']
+        x, y, z, r, _ = roi.astype(np.int32)
         com = circle(x, y, 1.2, anatomy_std[z].shape)
         if color_func:
             color = color_func(roi_id)
